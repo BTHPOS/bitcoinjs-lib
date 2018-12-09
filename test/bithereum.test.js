1297d97ca7e745c8b8fa46ad25afdb5082e73825
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-12-09 03:17:57 -0500

    Updated for travis fixes

--- test/bithereum.test.js
+++ test/bithereum.test.js
@@ -10,32 +10,7 @@ var Transaction = require('../src/transaction')
 
 describe('TransactionBuilder', function () {
   var network = NETWORKS['bithereum']
-  it('bethtestcase', function () {
-    var value = 50 * 1e8
-    var txid = '40c8a218923f23df3692530fa8e475251c50c7d630dccbdfbd92ba8092f4aa13'
-    var vout = 0
-
-    var wif = 'L54PmHcjKXi8H6v9cLAJ7DgGJFDpaFpR2YsV2WARieb82dz3QAfr'
-    var keyPair = ECPair.fromWIF(wif, network)
-
-    var pk = bcrypto.hash160(keyPair.getPublicKeyBuffer())
-    var spk = bscript.pubKeyHash.output.encode(pk)
-
-    var txb = new TransactionBuilder(network)
-    txb.addInput(txid, vout, Transaction.DEFAULT_SEQUENCE, spk)
-    txb.addOutput('GfEHv6hKvAX8HYfFzabMY2eiYDtC9eViqe', value)
-    txb.enableBithereum(true)
-    txb.setVersion(2)
-
-    var hashType = Transaction.SIGHASH_ALL | Transaction.SIGHASH_FORKID
-
-    txb.sign(0, keyPair, null, hashType, value)
-
-    var tx = txb.build()
-    var hex = tx.toHex()
-    assert.equal('020000000113aaf49280ba92bddfcbdc30d6c7501c2575e4a80f539236df233f9218a2c840000000006b483045022100c594c8e0750b1b6ec4e267b6d6c7098840f86fa9467f8aa452f439c3a72e0cd9022019759d800fffd7fcb78d16468f5693ea07a13da33607e0e8fbb4cdb5967075b441210201ad6a9a15457b162a71f1d5db8fe27ff001abc4ae3a888214f9407cb0da863cffffffff0100f2052a010000001976a914ea95bd5087d3b5f2df279304a46ad827225c4e8688ac00000000', hex)
-  })
-
+  
   it('bethtestcase_multisig_1', function () {
     var value = 50 * 1e8
 
